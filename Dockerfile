a9f4723580042f35473da59db0098a3be57d38f7
Ed Stephenson <edward.stephenson@me.com>
2020-09-27 17:45:08 +0000

    rotation

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
 FROM oznu/homebridge:latest
 
-ENV HOMEBRIDGE_CONFIG_UI=1
-ENV HOMEBRIDGE_CONFIG_UI_PORT=8080
-
-COPY /config/ /homebridge/
+COPY /config/ /homebridge
--- run.sh
+++
@@ -1,5 +0,0 @@
-docker run --net=host inesapp/homebridge
-
-ffmpeg -f video4linux2 -input_format h264 -video_size 1280x720 -framerate 30 -i /dev/video0 -vcodec copy -an test.h264
-
-"source": "-re -f video4linux2 -i /dev/video0",
